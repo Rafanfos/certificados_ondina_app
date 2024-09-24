97878534d9ea4b3e5444365ddb3bc4ef3b5cd48c
Rafael Barroso <r.barroso@pimenta.group>
2024-09-24 15:44:36 -0300

    Hotfix: fix toasts and csv upload

--- src/components/uploadCsv/index.tsx
+++ src/components/uploadCsv/index.tsx
@@ -1,10 +1,11 @@
 import { useStudents } from '@/contexts/StudentsContext';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { toast } from 'react-toastify';
 
 const UploadCSV = () => {
   const { uploadCSV } = useStudents();
   const [file, setFile] = useState<File | null>(null);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
 
   const handleDownloadTemplate = () => {
     const csvContent = 'Nome Completo,Trimestre\n';
@@ -24,16 +25,19 @@ const UploadCSV = () => {
 
   const handleUpload = async () => {
     if (!file) {
-      toast.error('Por favor, selecione um arquivo CSV.'); // Toast de erro
+      toast.error('Por favor, selecione um arquivo CSV.'); 
       return;
     }
 
     try {
       await uploadCSV(file);
-      toast.success('Arquivo CSV enviado com sucesso!'); // Toast de sucesso
       setFile(null);
+      if (fileInputRef.current) {
+        fileInputRef.current.value = '';
+      }
+      toast.success('Arquivo CSV enviado com sucesso!'); 
     } catch (error) {
-      toast.error('Erro ao enviar arquivo CSV, tente novamente.'); // Toast de erro
+      toast.error('Erro ao enviar arquivo CSV, tente novamente.'); 
     }
   };
 
@@ -53,6 +57,7 @@ const UploadCSV = () => {
         type='file'
         accept='.csv'
         onChange={handleFileChange}
+        ref={fileInputRef}
         className='block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 mb-4'
       />
       <button
